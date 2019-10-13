d8d7144f50e51b624df6fe7fc54a73efadb347b7
Chad Boyer <35845396+cboyer2016@users.noreply.github.com>
2019-10-13 16:34:15 -0400

    Add files via upload

---
+++ SeniorDesign/src/Main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Chad Boyer (cboyer2016@my.fit.edu), William Ferrick
+// 		(wferrick2016@my.fit.edu), Jordan Murray (jmurray2016@my.fit.edu), &
+//		Connor Roth (croth2016@my.fit.edu)
+// Course	   : CSE 4101
+// Description :
+//============================================================================
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Senior Design" << endl; // prints Senior Design
+	return 0;
+}
